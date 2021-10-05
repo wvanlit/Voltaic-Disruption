b548a10fc668e333916a9429ea6db1a77dc0bf9b
Wessel van Lit <wessel@litdev.nl>
2021-10-05 16:59:41 +0200

    ✨ Added locked door modes [ANY/ALL/HALF]

--- terrain/LockedDoor.gd
+++ terrain/LockedDoor.gd
@@ -2,17 +2,20 @@ extends StaticBody2D
 
 enum Mode { ANY, ALL, HALF }
 
-export var node_paths: Array
+export(Mode) var doorMode
+export var nodePaths: Array
 
 onready var animationPlayer = $AnimationPlayer
 
 var energyNodes = []
 var inputs = []
 
+var is_open = false
+
 
 func _ready() -> void:
-	for i in range(node_paths.size()):
-		var path = node_paths[i]
+	for i in range(nodePaths.size()):
+		var path = nodePaths[i]
 		var node = get_node(path)
 		if node is EnergyNode:
 			energyNodes.append(node as EnergyNode)
@@ -21,5 +24,40 @@ func _ready() -> void:
 
 
 func node_filled(node: EnergyNode):
-	animationPlayer.play("open")
-	pass
+	var index = energyNodes.find(node)
+
+	if index >= 0:
+		inputs[index] = true
+
+	check_status()
+
+
+func check_status():
+	var shouldOpen = should_door_open()
+
+	if shouldOpen and not is_open:
+		animationPlayer.play("open")
+		is_open = true
+
+
+func should_door_open() -> bool:
+	match doorMode:
+		Mode.ALL:
+			for input in inputs:
+				if not input:
+					return false
+			return true
+		Mode.ANY:
+			for input in inputs:
+				if input:
+					return true
+		Mode.HALF:
+			var half = inputs.size() / 2
+			var sum = 0
+
+			for input in inputs:
+				if input:
+					sum += 1
+
+			return sum >= half
+	return false
