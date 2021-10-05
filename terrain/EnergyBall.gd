e2ad08b906c0e4650d03340f209071981899e9a6
Wessel van Lit <wessel@litdev.nl>
2021-10-05 15:28:59 +0200

    🐛 Energy Ball now reappears at the orignial position if it hits terrain

--- terrain/EnergyBall.gd
+++ terrain/EnergyBall.gd
@@ -1,14 +1,30 @@
 class_name EnergyBall
 extends RigidBody2D
 
-onready var animationPlayer = $Sprite/AnimationPlayer
 const SPEED = 100
+var startingPosition: Vector2
+var shouldReset: bool = false
+
 
 func _ready() -> void:
-	gravity_scale = 0
+	startingPosition = position
+
+func _physics_process(delta: float) -> void:
+	if shouldReset:
+		resetPosition()
 
 func _on_area_entered(area: Area2D) -> void:
-	print('HIT')
 	var x_pos = self.global_position.x - area.global_position.x
 	var direction = 1 if x_pos > 0 else -1
-	set_linear_velocity(Vector2(direction*SPEED, 0))
+	set_linear_velocity(Vector2(direction * SPEED, 0))
+
+
+func _on_rigid_body_entered(body: Node) -> void:
+	if body is TileMap:
+		shouldReset = true
+
+
+func resetPosition():
+	set_linear_velocity(Vector2(0, 0))
+	shouldReset = false
+	position = startingPosition
