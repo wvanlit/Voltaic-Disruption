ee6d1ee1471dd0a4da731ede04d58f47cfcffe5f
Wessel van Lit <wessel@litdev.nl>
2021-10-05 16:02:13 +0200

    ✨ Added Energy Node which can be filled using an Energy Ball

---
+++ terrain/Energy Node.gd
@@ -0,0 +1,16 @@
+class_name EnergyNode
+extends RigidBody2D
+
+signal energy_filled
+
+onready var animationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animationPlayer.play("idle")
+
+func _on_body_entered(body: Node) -> void:
+	print('Hit')
+	if body is EnergyBall:
+		body.queue_free()
+		emit_signal("energy_filled")
+		animationPlayer.play("fill")
