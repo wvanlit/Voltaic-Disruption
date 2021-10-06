f77218aeee609a016f410e5778cbe6db13a7ea90
Wessel van Lit <wessel@litdev.nl>
2021-10-06 20:28:43 +0200

    🚧 Started on Pause Menu

--- levels/LevelManager.gd
+++ levels/LevelManager.gd
@@ -8,24 +8,48 @@ var currentLevelIndex = 0
 
 onready var globalEnv = $"/root/GlobalWorldEnvironment"
 
+var in_game_menu_scene = preload("res://ui/screens/In-Game Menu.tscn")
+var in_game_menu_instance = null
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
 		reset_level()
+	if event.is_action_pressed("menu"):
+		if in_game_menu_instance == null:
+			show_in_game_menu()
+		else:
+			hide_in_game_menu()
+
 
 func get_current_level() -> PackedScene:
 	globalEnv.set_mode(globalEnv.EnvMode.GAME)
-	return levels[currentLevelIndex] 
+	return levels[currentLevelIndex]
+
 
 func get_next_level() -> PackedScene:
 	currentLevelIndex += 1
 	if currentLevelIndex < levels.size():
 		globalEnv.set_mode(globalEnv.EnvMode.GAME)
-		return levels[currentLevelIndex] 
+		return levels[currentLevelIndex]
 	else:
 		globalEnv.set_mode(globalEnv.EnvMode.MENU)
 		currentLevelIndex = 0
 		return victory
 
+
 func reset_level():
 	# TODO - Add some animation
 	get_tree().reload_current_scene()
+
+
+func show_in_game_menu():
+	globalEnv.set_mode(globalEnv.EnvMode.MENU)
+	in_game_menu_instance = in_game_menu_scene.instance()
+	get_tree().get_root().add_child(in_game_menu_instance)
+
+
+func hide_in_game_menu():
+	globalEnv.set_mode(globalEnv.EnvMode.GAME)
+	in_game_menu_instance.queue_free()
+	in_game_menu_instance = null
---
+++ ui/screens/In-GameMenu.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+
+
+func _on_Back_To_Menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Resume_pressed() -> void:
+	pass # Replace with function body.
